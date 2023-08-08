dce4108ff7f968bb2097ed8d17e34c5bf366eab4
itstimi-XD <ssk0710402@gmail.com>
2023-08-08 17:27:58 +0900

    Feat: User Entity에 컬럼들 추가
    @Column 어노테이션 활용하여 해당 엔티티 필드가 DB에서 어떤 이름으로 생성될지, 길이는 얼마로 할 것인지 정해줌.
    
    related to: #1

--- src/main/java/com/mini/mbti_collector/domain/User.java
+++ src/main/java/com/mini/mbti_collector/domain/User.java
@@ -4,34 +4,35 @@ import jakarta.persistence.*;
 import java.time.LocalDateTime;
 import lombok.*;
 
-
 @Entity
-@Table(name = "User")
-@Getter
-@Builder
-@ToString(callSuper = true)
 @NoArgsConstructor
-@AllArgsConstructor
+@Table(name = "Users")
 public class User {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "user_idx")
     private int userIdx;
 
+    @Getter
     @Setter
+    @Column(name = "nickname", nullable = false, length = 50)
     private String nickname;
 
+    @Getter
     @Setter
-    @Column(unique = true)
+    @Column(name = "email", nullable = false, length = 255)
     private String email;
 
     @Setter
+    @Column(name = "password", nullable = false, length = 60)
     private String password;
 
     @Column(name = "createdAt", nullable = false, columnDefinition = "TIMESTAMP")
     private LocalDateTime createdAt;
 
+    @Getter
     @Setter
-    private Boolean deleted;
-
+    @Column(name = "deleted", nullable = false, length = 1)
+    private String deleted;
 }
