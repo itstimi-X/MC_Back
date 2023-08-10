a4ecdc228d3597da4c9255ce56f1a7a600bff6de
itstimi-XD <ssk0710402@gmail.com>
2023-08-10 15:20:20 +0900

    Refactor: 불필요한 주석과 불필요한 코드 제거
    원래는 이메일 인증번호 전송 API(api/email-verification)을 usercontroller에서 처리하려했으나,  MailController를 생성하여 분리함에 따라 해당 내용 삭제함.
    
    related to: #5

--- src/main/java/com/mini/mbti_collector/controller/UserController.java
+++ src/main/java/com/mini/mbti_collector/controller/UserController.java
@@ -1,29 +1,10 @@
 package com.mini.mbti_collector.controller;
 
-import com.mini.mbti_collector.service.MailService;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpSession;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/users")
 @CrossOrigin(origins = "http://localhost:5500")
 public class UserController {
-    MailService mailService;
-    @Autowired
-    public UserController(MailService mailService) {
-        this.mailService = mailService;
-    }
-    @PostMapping(value = "/email-verification")
-    private int sendEmail(HttpServletRequest request, @RequestParam String userEmail) {
-        HttpSession session = request.getSession();
-        mailService.mailSend(session, userEmail);
-        return 123;
-    }
+
 }
