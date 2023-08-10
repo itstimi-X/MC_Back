72e9254fe89a2dd513e0f1e23ba2941fdf75bf77
itstimi-XD <ssk0710402@gmail.com>
2023-08-10 14:13:23 +0900

    Feat: SecurityConfig 생성 및 구현
    cors, csrf 관련된 설정의 예제 대다수가 Deprecated된 표현으로, 
    * https://docs.spring.io/spring-security/reference/migration-7/configuration.html#_use_the_lambda_dsl
    
    해당 문서 참고하여 deprecated 되지 않은 표현식으로 설정함.
    related to: #5

---
+++ src/main/java/com/mini/mbti_collector/config/SecurityConfig.java
@@ -0,0 +1,35 @@
+package com.mini.mbti_collector.config;
+
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    protected PasswordEncoder passwordEncoder() {
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests((authz) -> authz
+                        .anyRequest().permitAll()
+                )
+                .httpBasic(Customizer.withDefaults())
+                .csrf(csrf -> csrf.disable());
+        return http.build();
+    }
+
+}
