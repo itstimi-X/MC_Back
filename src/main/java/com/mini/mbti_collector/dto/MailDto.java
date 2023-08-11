f82dcd27df4ce3d659536e58ac9993d222ce24fb
itstimi-XD <ssk0710402@gmail.com>
2023-08-11 15:21:32 +0900

    Feat: 정규표현식을 이용한 이메일 검증
    그렇게 되면 @Email 어노테이션과 @Pattern을 사용하여 이메일 형식을 체크하고 있기 때문에, @Size 어노테이션은 중복으로 작용할 수 있음. 따라서 @Size 어노테이션은 제거
    
    related to: #5

--- src/main/java/com/mini/mbti_collector/dto/MailDto.java
+++ src/main/java/com/mini/mbti_collector/dto/MailDto.java
@@ -2,6 +2,7 @@ package com.mini.mbti_collector.dto;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -15,7 +16,7 @@ public class MailDto {
     public static class Request {
         @NotBlank(message = "Email should not be blank.") // 문자열이 null이거나 빈 값(empty)이 아닌지 검사합니다.
         @Email(message = "Invalid email format.") // 문자열이 유효한 이메일 형식인지 검사합니다.
-        @Size(min = 1, max = 255, message = "Email length should be between 1 and 255 characters.") // DB 스키마에 따라 email 필드의 최대 길이가 255자로 정의되어 있으므로, 이에 대한 검증을 DTO에서도 적용. 이렇게 DTO에서 길이 제한을 검증하면, 사용자가 255자를 초과하는 긴 이메일 주소를 입력하려고 할 때 요청 단계에서 오류를 반환하여 DB에 저장되기 전에 문제를 잡아낼 수 있음.
+        @Pattern(regexp = "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,6}$", message = "Invalid email format.")
         private String email;
     }
 
