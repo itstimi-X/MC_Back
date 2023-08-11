a4a125a8a2c9797e7e4069f7851d0f5e84a95d35
itstimi-XD <ssk0710402@gmail.com>
2023-08-11 16:22:59 +0900

    Refactor: CORS 중복설정 제거
    - WebConfig에서의 CORS 설정을 SecurityConfig로 통합
    - 중복된 CORS 설정으로 인한 혼란 방지
    - 불필요한 WebConfig 의 내용 삭제
    - UserController에도 cors 설정 있을 필요 없으므로 삭제
    
    related to: #5

--- src/main/java/com/mini/mbti_collector/config/WebConfig.java
+++ src/main/java/com/mini/mbti_collector/config/WebConfig.java
@@ -6,12 +6,5 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
-    @Override
-    public void addCorsMappings(CorsRegistry corsRegistry) {
-        corsRegistry.addMapping("/**")
-                .allowedOrigins("chrome-extension://fhbjgbiflinjbdggehcddcbncdddomop","http://localhost:5500")
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "HEAD", "OPTIONS", "PATCH")
-                .allowCredentials(true)
-                .maxAge(3600L);
-    }
+    
 }
