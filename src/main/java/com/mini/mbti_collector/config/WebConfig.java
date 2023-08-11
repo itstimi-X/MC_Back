55d7b862cb8bfae3df085b8bc7abbc8a69f63809
itstimi-XD <ssk0710402@gmail.com>
2023-08-11 16:23:32 +0900

    Refactor: 불필요한 annotation 삭제
    related to: #5

--- src/main/java/com/mini/mbti_collector/config/WebConfig.java
+++ src/main/java/com/mini/mbti_collector/config/WebConfig.java
@@ -1,10 +1,9 @@
 package com.mini.mbti_collector.config;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
-    
+
 }
