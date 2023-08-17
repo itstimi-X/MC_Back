eac32da55b524c129c015598fd4bfa2540e288fa
itstimi-XD <ssk0710402@gmail.com>
2023-08-17 17:10:49 +0900

    Feat: CORS 설정 update
    related to: #6

--- src/main/java/com/mini/mbti_collector/config/SecurityConfig.java
+++ src/main/java/com/mini/mbti_collector/config/SecurityConfig.java
@@ -1,6 +1,5 @@
 package com.mini.mbti_collector.config;
 
-import java.util.Arrays;
 import java.util.List;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -10,9 +9,12 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.access.channel.ChannelProcessingFilter;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
@@ -23,7 +25,8 @@ public class SecurityConfig {
     }
 
     @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+    public SecurityFilterChain filterChain(HttpSecurity http, CorsFilter corsFilter) throws Exception {
+
         http
                 .authorizeHttpRequests((authz) -> authz
                         .anyRequest().permitAll()
@@ -36,14 +39,15 @@ public class SecurityConfig {
                         throw new RuntimeException(e);
                     }
                 })
-                .csrf(csrf -> csrf.disable());
+                .csrf(csrf -> csrf.disable())
+                .addFilterBefore(corsFilter, ChannelProcessingFilter.class); // CORS 필터 추가
         return http.build();
     }
 
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("chrome-extension://fhbjgbiflinjbdggehcddcbncdddomop", "http://127.0.0.1:5500"));
+        configuration.setAllowedOrigins(List.of("http://127.0.0.1:5500","https://localhost:8080"));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "HEAD", "OPTIONS", "PATCH"));
         configuration.setAllowCredentials(true); // 쿠키 전송을 허용합니다.
         configuration.setAllowedHeaders(List.of("Authorization", "Cache-Control", "Content-Type"));
@@ -54,4 +58,10 @@ public class SecurityConfig {
 
         return source;
     }
+
+    @Bean
+    public CorsFilter corsFilter(CorsConfigurationSource corsConfigurationSource) {
+        return new CorsFilter(corsConfigurationSource);
+    }
+
 }
