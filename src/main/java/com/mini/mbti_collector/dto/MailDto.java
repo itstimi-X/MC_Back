f0637fe9d025225aa7fd12bc50c72e4e6a179faf
itstimi-XD <ssk0710402@gmail.com>
2023-08-17 18:22:12 +0900

    Refactor: valid-message 내용 한국말로 변경

--- src/main/java/com/mini/mbti_collector/dto/MailDto.java
+++ src/main/java/com/mini/mbti_collector/dto/MailDto.java
@@ -15,9 +15,9 @@ public class MailDto {
     @NoArgsConstructor // 파라미터가 없는 기본 생성자를 자동으로 생성합니다.
     @AllArgsConstructor // 모든 필드 값을 파라미터로 받는 생성자를 자동으로 생성합니다.
     public static class Request {
-        @NotBlank(message = "Email should not be blank.") // 문자열이 null이거나 빈 값(empty)이 아닌지 검사합니다.
-        @Email(message = "Invalid email format.") // 문자열이 유효한 이메일 형식인지 검사합니다.
-        @Pattern(regexp = "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,6}$", message = "Invalid email format.")
+        @NotBlank(message = "이메일 주소를 입력해주세요.")
+        @Email(message = "올바른 이메일 주소를 입력해주세요.")
+        @Pattern(regexp = "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,6}$", message = "올바른 이메일 주소를 입력해주세요.")
         private String email;
     }
 
