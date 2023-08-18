7f3dc8a268b8610a9043373066987df7056ab999
itstimi-XD <ssk0710402@gmail.com>
2023-08-18 10:53:06 +0900

    Fix: url수정 username -> nickname
    @GetMapping("/api/users/check-nickname/{nickname}")

--- src/main/java/com/mini/mbti_collector/controller/UserController.java
+++ src/main/java/com/mini/mbti_collector/controller/UserController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserController {
 
     private final UserService userService;
-    @GetMapping("/api/users/check-username/{nickname}")
+    @GetMapping("/api/users/check-nickname/{nickname}")
     public ResponseEntity<Boolean> isNicknameRegistered(@PathVariable String nickname) {
         return ResponseEntity.ok(userService.isNicknameRegistered(nickname));
     }
