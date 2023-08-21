6848901d507ade3bae6d00ed943b3acbbf1c150a
itstimi-XD <ssk0710402@gmail.com>
2023-08-21 15:34:34 +0900

    Feat: UserDto 코드에 User type return toEntity()메소드 추가
    related to: #7

--- src/main/java/com/mini/mbti_collector/dto/UserDto.java
+++ src/main/java/com/mini/mbti_collector/dto/UserDto.java
@@ -1,24 +1,44 @@
 package com.mini.mbti_collector.dto;
 
+import com.mini.mbti_collector.domain.User;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
+import java.time.LocalDateTime;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Getter
+@Setter
+@NoArgsConstructor // 파라미터가 없는 기본 생성자를 자동으로 생성합니다.
+@AllArgsConstructor // 모든 필드 값을 파라미터로 받는 생성자를 자동으로 생성합니다.
 public class UserDto {
-    @NotBlank(message = "닉네임을 입력해주세요.")
-    @Size(min = 2, max = 10, message = "닉네임은 2자 이상 10자 이하로 입력해주세요.")
-    private String nickname;
 
-    @NotBlank(message = "이메일 주소를 입력해주세요.")
-    @Email(message = "올바른 이메일 주소를 입력해주세요.")
-    @Pattern(regexp = "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,6}$", message = "Invalid email format.")
-    private String email;
 
-    @NotBlank(message = "비밀번호를 입력해주세요.")
-    @Size(min = 8, max = 20, message = "비밀번호는 8자 이상 20자 이하로 입력해주세요.")
-    private String password;
+        @NotBlank(message = "닉네임을 입력해주세요.")
+        @Size(min = 2, max = 10, message = "닉네임은 2자 이상 10자 이하로 입력해주세요.")
+        private String nickname;
+
+        @NotBlank(message = "이메일 주소를 입력해주세요.")
+        @Email(message = "올바른 이메일 주소를 입력해주세요.")
+        @Pattern(regexp = "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,6}$", message = "Invalid email format.")
+        private String email;
+
+        @NotBlank(message = "비밀번호를 입력해주세요.")
+        @Size(min = 8, max = 20, message = "비밀번호는 8자 이상 20자 이하로 입력해주세요.")
+        private String password;
+
+        public User toEntity() {
+            User user = new User();
+            user.setNickname(this.nickname);
+            user.setEmail(this.email);
+            user.setPassword(this.password);
+            user.setCreatedAt(LocalDateTime.now()); // createdAt 필드를 현재 시간으로 설정
+            user.setDeleted("N"); // deleted 필드를 설정
+            return user;
+        }
 
 }
