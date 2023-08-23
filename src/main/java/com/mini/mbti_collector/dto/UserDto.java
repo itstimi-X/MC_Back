0fc08f74a4e87c3dacd8953585a321408118c64f
itstimi-X <ihkim@acrofuture.com>
2023-08-23 20:19:12 +0900

    Feat: UserDto에 loginRequest와 loginResponse DTO 클래스를 추가
    related to: #10

--- src/main/java/com/mini/mbti_collector/dto/UserDto.java
+++ src/main/java/com/mini/mbti_collector/dto/UserDto.java
@@ -31,6 +31,23 @@ public class UserDto {
         @Size(min = 8, max = 20, message = "비밀번호는 8자 이상 20자 이하로 입력해주세요.")
         private String password;
     }
+
+    @Getter
+    @Setter
+    @NoArgsConstructor // 파라미터가 없는 기본 생성자를 자동으로 생성합니다.
+    @AllArgsConstructor // 모든 필드 값을 파라미터로 받는 생성자를 자동으로 생성합니다.
+    public static class loginRequest {
+        private String email;
+        private String password;
+    }
+
+    @Getter
+    @Setter
+    @NoArgsConstructor // 파라미터가 없는 기본 생성자를 자동으로 생성합니다.
+    @AllArgsConstructor // 모든 필드 값을 파라미터로 받는 생성자를 자동으로 생성합니다.
+    public static class loginResponse {
+        private String accessToken;
+    }
 //        public User toEntity() {
 //            User user = new User();
 //            user.setNickname(this.nickname);
