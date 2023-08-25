e42d5b663a6b94319ddf2bff775b46dbb1326db4
itstimi-X <ihkim@acrofuture.com>
2023-08-25 16:52:20 +0900

    Refactor: User Entity Annotation 형식 정리
    related to: #1

--- src/main/java/com/mini/mbti_collector/domain/User.java
+++ src/main/java/com/mini/mbti_collector/domain/User.java
@@ -21,30 +21,27 @@ public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "user_idx")
+    @Getter
     private int userIdx;
 
-    @Getter
-    @Setter
+
     @Column(name = "nickname", nullable = false, length = 50)
+    @Getter @Setter
     private String nickname;
 
-    @Getter
-    @Setter
     @Column(name = "email", nullable = false, length = 255)
+    @Getter @Setter
     private String email;
 
-    @Setter
-    @Getter
     @Column(name = "password", nullable = false, length = 60)
+    @Getter @Setter
     private String password;
 
-    @Setter
     @Column(name = "createdAt", nullable = false, columnDefinition = "TIMESTAMP")
     private LocalDateTime createdAt;
 
-    @Getter
-    @Setter
     @Column(name = "deleted", nullable = false, length = 1)
+    @Getter @Setter
     private String deleted;
 
     public static User of(String nickname, String email, String password) {
