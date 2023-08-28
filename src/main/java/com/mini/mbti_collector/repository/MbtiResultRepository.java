df6775e721c06e4daaffe72acd557ac5fa211425
itstimi-X <ihkim@acrofuture.com>
2023-08-28 15:55:34 +0900

    Feat: Repository에 extends JPA 추가
    related to: #11

--- src/main/java/com/mini/mbti_collector/repository/MbtiResultRepository.java
+++ src/main/java/com/mini/mbti_collector/repository/MbtiResultRepository.java
@@ -1,2 +1,8 @@
-package com.mini.mbti_collector.repository;public class MbtiResultRepository {
+package com.mini.mbti_collector.repository;
+
+import com.mini.mbti_collector.domain.MbtiResult;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MbtiResultRepository extends JpaRepository<MbtiResult, Integer> {
+
 }
