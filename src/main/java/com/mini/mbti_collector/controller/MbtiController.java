ab156af6cc8da2fdd525ba14328a80b8431075d7
itstimi-X <ihkim@acrofuture.com>
2023-08-29 14:55:07 +0900

    Feat:  에러 처리 및 적절한 응답 코드 전송
    related to: #11

--- src/main/java/com/mini/mbti_collector/controller/MbtiController.java
+++ src/main/java/com/mini/mbti_collector/controller/MbtiController.java
@@ -1,8 +1,11 @@
 package com.mini.mbti_collector.controller;
 
 import com.mini.mbti_collector.dto.MbtiDto;
+import com.mini.mbti_collector.exception.CustomAuthenticationException;
 import com.mini.mbti_collector.service.MbtiService;
 import com.mini.mbti_collector.service.MbtiServiceImpl;
+import jakarta.validation.Valid;
+import java.util.Map;
 import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -20,14 +23,16 @@ public class MbtiController {
     private final MbtiService mbtiService;
     private static final Logger logger = LoggerFactory.getLogger(MbtiServiceImpl.class);
     @PostMapping("/mbti/save")
-    public ResponseEntity<?> saveMbtiResult(@RequestHeader("Authorization") String authorizationHeader, @RequestBody MbtiDto.Request request) {
+    public ResponseEntity<?> saveMbtiResult(@RequestHeader("Authorization") String authorizationHeader, @RequestBody @Valid MbtiDto.Request request) {
         logger.info("ePercent: {}, nPercent: {}, tPercent: {}, jPercent: {}",
                 request.getEPercent(), request.getNPercent(), request.getTPercent(), request.getJPercent());
         try {
             mbtiService.saveMbtiResult(authorizationHeader, request);
-            return new ResponseEntity<>(HttpStatus.OK);
+            return new ResponseEntity<>(Map.of("success", true, "message", "MBTI 결과가 성공적으로 저장되었습니다."), HttpStatus.OK);
+        } catch (CustomAuthenticationException e) {
+            return new ResponseEntity<>(Map.of("success", false, "message", e.getMessage()), HttpStatus.UNAUTHORIZED);
         } catch (Exception e) {
-            return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
+            return new ResponseEntity<>(Map.of("success", false, "message", e.getMessage()), HttpStatus.BAD_REQUEST);
         }
     }
 
