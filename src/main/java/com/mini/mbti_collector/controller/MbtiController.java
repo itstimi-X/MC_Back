38ede5a0c2c7d432a61e7c7ffbe9f003ef25b709
itstimi-X <ihkim@acrofuture.com>
2023-08-29 14:08:01 +0900

    Feat: 서비스 클래스에 SLF4J 로거 추가
    - dto에 값이 제대로 안들어오는 지 확인 위한 로거 추가
    
    related to: #11

--- src/main/java/com/mini/mbti_collector/controller/MbtiController.java
+++ src/main/java/com/mini/mbti_collector/controller/MbtiController.java
@@ -2,7 +2,10 @@ package com.mini.mbti_collector.controller;
 
 import com.mini.mbti_collector.dto.MbtiDto;
 import com.mini.mbti_collector.service.MbtiService;
+import com.mini.mbti_collector.service.MbtiServiceImpl;
 import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -15,9 +18,11 @@ import org.springframework.web.bind.annotation.RestController;
 public class MbtiController {
 
     private final MbtiService mbtiService;
-
+    private static final Logger logger = LoggerFactory.getLogger(MbtiServiceImpl.class);
     @PostMapping("/mbti/save")
     public ResponseEntity<?> saveMbtiResult(@RequestHeader("Authorization") String authorizationHeader, @RequestBody MbtiDto.Request request) {
+        logger.info("ePercent: {}, nPercent: {}, tPercent: {}, jPercent: {}",
+                request.getEPercent(), request.getNPercent(), request.getTPercent(), request.getJPercent());
         try {
             mbtiService.saveMbtiResult(authorizationHeader, request);
             return new ResponseEntity<>(HttpStatus.OK);
