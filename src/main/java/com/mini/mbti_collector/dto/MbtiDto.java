aebe1956b88f050cf7b90eddefccc5a9671e2aba
itstimi-X <ihkim@acrofuture.com>
2023-08-29 15:03:37 +0900

    Feat: 필드별 유효성 검사 추가
    - MbtiDto.Request 클래스의 각 필드에 0~100 사이의 정수 값 유효성 검사 어노테이션 추가
    - 컨트롤러 메서드에 @Valid 어노테이션 추가하여 요청시 유효성 검사 수행
    
    related to: #11

--- src/main/java/com/mini/mbti_collector/dto/MbtiDto.java
+++ src/main/java/com/mini/mbti_collector/dto/MbtiDto.java
@@ -1,6 +1,9 @@
 package com.mini.mbti_collector.dto;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -13,13 +16,27 @@ public class MbtiDto {
     @AllArgsConstructor
     public static class Request {
         @JsonProperty("e_percent")
+        @NotNull(message = "ePercent 필드는 비어있을 수 없습니다.")
+        @Min(value = 0, message = "ePercent 값은 0 이상이어야 합니다.")
+        @Max(value = 100, message = "ePercent 값은 100 이하여야 합니다.")
         private int ePercent;
+
         @JsonProperty("n_percent")
+        @NotNull(message = "nPercent 필드는 비어있을 수 없습니다.")
+        @Min(value = 0, message = "nPercent 값은 0 이상이어야 합니다.")
+        @Max(value = 100, message = "nPercent 값은 100 이하여야 합니다.")
         private int nPercent;
+
         @JsonProperty("t_percent")
+        @NotNull(message = "tPercent 필드는 비어있을 수 없습니다.")
+        @Min(value = 0, message = "tPercent 값은 0 이상이어야 합니다.")
+        @Max(value = 100, message = "tPercent 값은 100 이하여야 합니다.")
         private int tPercent;
+
         @JsonProperty("j_percent")
+        @NotNull(message = "jPercent 필드는 비어있을 수 없습니다.")
+        @Min(value = 0, message = "jPercent 값은 0 이상이어야 합니다.")
+        @Max(value = 100, message = "jPercent 값은 100 이하여야 합니다.")
         private int jPercent;
-//        private String resultMbti;
     }
 }
