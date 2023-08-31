96f12123c8de17f5f0820236e83a72acacf28c0f
itstimi-X <ihkim@acrofuture.com>
2023-08-31 18:34:57 +0900

    Feat: MBTI 날짜별 레이더 차트 조회
    - 사용자의 날짜별 MBTI 수치를 데이터베이스에서 조회하는 로직 구현
    - 레이더 차트 형식으로 데이터 변환 로직 추가
    - 데이터가 없을 경우 `NoDataFoundException` 예외 처리 추가
    
    related to: #13

--- src/main/java/com/mini/mbti_collector/service/MbtiService.java
+++ src/main/java/com/mini/mbti_collector/service/MbtiService.java
@@ -2,9 +2,12 @@ package com.mini.mbti_collector.service;
 
 import com.mini.mbti_collector.dto.MbtiDto;
 import com.mini.mbti_collector.dto.MbtiDto.Response;
+import java.util.List;
 
 public interface MbtiService {
     public void saveMbtiResult(String authorizationHeader, MbtiDto.Request request) throws Exception;
 
     Response getLatestMbtiResult(String authorizationHeader) throws Exception;
+
+    List<Response> getRadarChartData(String authorizationHeader) throws Exception;
 }
