2749ddda006ee5036ba5049bf31d626779f6f9d0
itstimi-X <ihkim@acrofuture.com>
2023-08-31 17:55:45 +0900

    Feat: 커스텀 예외 클래스 생성
    - 데이터를 조회하고, 그 결과가 비어 있을 경우 예외를 발생시킨다.
    
    related to: #13

---
+++ src/main/java/com/mini/mbti_collector/exception/NoDataFoundException.java
@@ -0,0 +1,8 @@
+package com.mini.mbti_collector.exception;
+
+public class NoDataFoundException extends RuntimeException {
+    public NoDataFoundException(String message) {
+        super(message);
+    }
+}
+
